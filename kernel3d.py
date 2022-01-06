3d931e0474e9ce2e777616df1bd57e7ab86f9d43
Roberto Esposito <roberto.esposito@unito.it>
2022-01-06 14:51:25 +0100

    First complete script

--- kernel3d.py
+++ kernel3d.py
@@ -1,25 +1,50 @@
 from manim import *
 import numpy as np
 from numpy.linalg import inv
+import math
 
 
 class Kernel3D(ThreeDScene):
-    NUM_EXAMPLES = 5
-    QUAD_SP = [0.3, 0.3] # Quadratic surface scaling parameters
+    NUM_EXAMPLES = 30
+    QUAD_SP = 0.3 # Quadratic surface scaling parameters
     LABELING_THRESHOLD = 1.5
+    RANDOM_SEED=42
 
     def construct(self):
+        np.random.seed(self.RANDOM_SEED)
         self.add_axes()
         self.add_data_points()
+        self.wait(2)
 
         self.animate_move_camera()
+
+        kernel_text = Tex(r"$(x,y) \rightarrow (x^2, y^2)$", font_size=72)
+        kernel_text.to_edge(UP, buff=0.5)
+        kernel_text.to_edge(RIGHT, buff=0.5)
+        self.add_fixed_in_frame_mobjects(kernel_text)
+        self.play(Write(kernel_text))
+
         kernel_animations = self.apply_kernel()
         quad_surf_animation = self.add_quadratic_surface()
-
         self.play(*kernel_animations, quad_surf_animation, run_time=5)
+
+        self.play(Unwrite(kernel_text))
+        self.wait(1)
         
-        linear_classifier = self.add_linear_classifier()
-        self.play(linear_classifier)
+        linear_classifier_anim = self.add_linear_classifier()
+        self.play(linear_classifier_anim)
+
+        intersection_line = self.add_intersection_line()
+        self.play(intersection_line)
+        self.move_camera(np.pi/2, -0.45*np.pi)
+        self.wait(2)
+        self.move_camera(0, 0)
+
+        self.play(Uncreate(self.quadratic_surface), Uncreate(self.linear_surface))
+        self.remove(self.quadratic_surface)
+        self.play(Uncreate(self.linear_classifier))
+        self.remove(self.linear_classifier)
+        self.wait(2)
 
     def animate_move_camera(self):
         # Static movement of the camera (for debugging purposes)
@@ -41,7 +66,7 @@ class Kernel3D(ThreeDScene):
         def labeling_rule(x, y): return 1 if (x**2 + y**2) > self.LABELING_THRESHOLD else -1
 
         for coord in self.data_coords:
-            self.data_points.append(Sphere(center = coord,radius = 0.05))
+            self.data_points.append(Sphere(center = coord, radius = 0.05))
 
             label = labeling_rule(coord[0], coord[1])
             self.labels.append(label)
@@ -50,7 +75,7 @@ class Kernel3D(ThreeDScene):
         self.add(*self.data_points)
 
     def quadratic_kernel(self, x, y):
-        return self.QUAD_SP[0] * x**2 + self.QUAD_SP[1] * y**2
+        return self.QUAD_SP * x**2 + self.QUAD_SP * y**2
 
     def apply_kernel(self):
         animations = []
@@ -69,6 +94,9 @@ class Kernel3D(ThreeDScene):
         linear_surface.set_fill_by_value(
             axes=self.axes, colors=[(RED, -0.4), (YELLOW, 0), (GREEN, 0.4)])
 
+        self.linear_surface = linear_surface
+        self.play(Create(linear_surface))
+
         quadratic_surface = Surface(
             lambda x, y: self.axes.c2p(x, y,  self.quadratic_kernel(x,y)),
             v_range=[-5, 5],
@@ -77,13 +105,13 @@ class Kernel3D(ThreeDScene):
         quadratic_surface.set_style(fill_opacity=0.3)
         quadratic_surface.set_fill_by_value(axes=self.axes, colors=[(RED, -0.4), (YELLOW, 0), (GREEN, 0.4)])
 
-        self.add(linear_surface)
+        self.quadratic_surface = quadratic_surface
         return Transform(linear_surface, quadratic_surface)
 
         # self.play(self.add(self.axes, quadratic_surface))
 
     def add_linear_classifier(self):
-        classify = lambda x, y: self.LABELING_THRESHOLD
+        classify = lambda x, y: self.LABELING_THRESHOLD * self.QUAD_SP 
 
         linear_classifier = Surface(
             lambda x, y: self.axes.c2p(x, y, classify(x, y)),
@@ -95,15 +123,17 @@ class Kernel3D(ThreeDScene):
         linear_classifier.set_fill_by_value(
             axes=self.axes, colors=[(RED, -0.4), (YELLOW, 0), (GREEN, 0.4)])
 
-        xy_plane = Surface(
-            lambda x, y: self.axes.c2p(x, y, 0),
+        self.linear_classifier = linear_classifier
+        return Create(linear_classifier)
+
+    def add_intersection_line(self):
+        intersection_line = Circle(
+            radius = math.sqrt(self.LABELING_THRESHOLD-0.2),
+            color=RED
         )
 
-        xy_plane.set_style(fill_opacity=0.3)
-        xy_plane.set_fill_by_value(
-            axes=self.axes, colors=[(RED, -0.4), (YELLOW, 0), (GREEN, 0.4)])
-        self.add(xy_plane)
+        intersection_line.shift(self.axes.c2p(0, 0, self.LABELING_THRESHOLD * self.QUAD_SP))
 
-        return Transform(xy_plane, linear_classifier)
-
+        self.add(intersection_line)
+        return DrawBorderThenFill(intersection_line)
 
